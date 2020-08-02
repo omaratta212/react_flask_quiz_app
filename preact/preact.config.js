b777492c3729e273e2f1fdd17d365f92c2ac63c7
Omar Atta <omaratta212@gmail.com>
2020-08-02 07:10:06 +0200

    COnfigure terser plugin

--- preact/preact.config.js
+++ preact/preact.config.js
@@ -1,6 +1,9 @@
 import tailwind from "preact-purge-tailwindcss";
 
 module.exports = (config, env, helpers) => {
+    if(config.optimization && config.optimization.minimizer){
+        config.optimization.minimizer[0].options.extractComments = false;
+    }
     config = tailwind(config, env, helpers);
     return config;
 };
